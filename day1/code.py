6d7709146006b651891af4cda0646dd2a27fb511
StanleyTack <stanley.tack@gmail.com>
2024-12-02 14:14:25 -0800

    day 1 part2 and move input files

--- day1/code.py
+++ day1/code.py
@@ -1,3 +1,5 @@
+DAY = 'day_1'
+
 def read_file(filename):
     """Reads the contents of a file and returns it as a list of lines."""
     with open(filename, 'r') as file:
@@ -6,7 +8,7 @@ def read_file(filename):
 
 def part_a():
     # filename = 'sample.txt'
-    filename = 'input.txt'
+    filename = f'../inputs/{DAY}_input.txt'
     lines = read_file(filename)
     list1 = []
     list2 = []
@@ -20,17 +22,39 @@ def part_a():
     for i, num in enumerate(list1):
         distance = abs(num - list2[i])
         distances.append(distance)
-    print(sum(distances))
+    print(f"Day 1 Part A: {sum(distances)}")
+
     # pass
 
 def part_b():
     # filename = 'sample.txt'
-    # filename = 'input.txt'
-    pass
+    filename = f'../inputs/{DAY}_input.txt'
+    lines = read_file(filename)
+    list1 = []
+    list2 = []
+    for line in lines:
+        chars = line.split()
+        list1.append(int(chars[0]))
+        list2.append(int(chars[1]))
+    similarities = []
+    counted = set()
+    for num1 in list1:
+        counted.add(num1)
+        num_count = 0
+        for num2 in list2:
+            if num2 == num1:
+                num_count += 1
+        similarities.append((num1, num_count))
+    sim_scores = []
+    for pair in similarities:
+        sim_score = pair[0] * pair[1]
+        sim_scores.append(sim_score)
+    print(f"Day 1 Part B: {sum(sim_scores)}")
+    # pass
 
 def main():
     part_a()
-    # part_b()
+    part_b()
 
 if __name__ == "__main__":
     main()
