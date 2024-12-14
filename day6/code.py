8749758e4d601cbbad1a1d61a984dfca535b895a
StanleyTack <stanley.tack@gmail.com>
2024-12-13 16:56:40 -0800

    day7 part 1

--- day6/code.py
+++ day6/code.py
@@ -24,8 +24,119 @@ def turn_right(patrol_dir):
     patrol_dir += 90
     if patrol_dir == 360:
         patrol_dir = 0
-    print(f"Direction {patrol_dir}")
+    # print(f"Direction {patrol_dir}")
     return patrol_dir
+
+def count_grid_dir(grid_dir):
+    non_none_count = sum(1 for row in grid_dir for element in row if element is not None)
+    non_none_sum = sum(element for row in grid_dir for element in row if element is not None)
+    return non_none_count, non_none_sum
+
+
+def traverse_grid(grid, grid_size, grid_dir, obstacle):
+    guard_present = True
+    guard_position = find_start_coord(grid)
+    patrol_dir = 0
+    steps = 0
+    same_dir = False
+    non_none_count = 1
+    while guard_present and same_dir == False and steps < 1000:
+        steps += 1
+        new_non_none_count, _ = count_grid_dir(grid_dir)
+        if new_non_none_count == non_none_count:
+            # print(f"same count at {obstacle}: {non_none_count, new_non_none_count}")
+            break
+        else:
+            print(print(obstacle, non_none_count))
+        if patrol_dir == 0: # guard moving up
+            x = guard_position[0]
+            y = guard_position[1]
+            for y in range(y, -1, -1):
+                ele = grid[y][x]
+                ele_dir = grid_dir[y][x]
+                if ele_dir == patrol_dir:
+                    same_dir = True
+                    break
+                # print(x, y, ele)
+                if ele == '#':
+                    guard_position = (x, y+1)
+                    # print(f"gaurd position {guard_position}")
+                    patrol_dir = turn_right(patrol_dir)
+                    break
+                grid[y][x] = '1'
+                grid_dir[y][x] = patrol_dir
+                if ele == '.' and y == 0:
+                    guard_present = False
+                    break
+        if patrol_dir == 180: # guard moving down
+            x = guard_position[0]
+            y = guard_position[1]
+            for y in range(y, grid_size, 1):
+                ele = grid[y][x]
+                ele_dir = grid_dir[y][x]
+                if ele_dir == patrol_dir:
+                    same_dir = True
+                    break
+                # print(x, y, ele)
+                if ele == '#':
+                    guard_position = (x, y-1)
+                    patrol_dir = turn_right(patrol_dir)
+                    break
+                grid[y][x] = '1'
+                grid_dir[y][x] = patrol_dir
+                if ele == '.' and y == grid_size-1:
+                    guard_present = False
+                    break
+        if patrol_dir == 90: # guard moving right
+            x = guard_position[0]
+            y = guard_position[1]
+            for x in range(x, grid_size, 1):
+                ele = grid[y][x]
+                ele_dir = grid_dir[y][x]
+                if ele_dir == patrol_dir:
+                    same_dir = True
+                    break
+                # print(x, y, ele)
+                if ele == '#':
+                    guard_position = (x-1, y)
+                    patrol_dir = turn_right(patrol_dir)
+                    break
+                grid[y][x] = '1'
+                grid_dir[y][x] = patrol_dir
+                if ele == '.' and x == grid_size-1:
+                    guard_present = False
+                    break
+        if patrol_dir == 270: # guard moving left
+            x = guard_position[0]
+            y = guard_position[1]
+            for x in range(x, -1, -1):
+                ele = grid[y][x]
+                ele_dir = grid_dir[y][x]
+                if ele_dir == patrol_dir:
+                    same_dir = True
+                    break
+                # print(x, y, ele)
+                if ele == '#':
+                    guard_position = (x+1, y)
+                    patrol_dir = turn_right(patrol_dir)
+                    break
+                grid[y][x] = '1'
+                grid_dir[y][x] = patrol_dir
+                if ele == '.' and x == 0:
+                    guard_present = False
+                    break
+                
+    # for line in grid:
+    #     print(line)
+    # for line in grid_dir:
+    #     print(line)
+
+    if not guard_present:
+        # print("Guard departed map")
+        return False
+    if same_dir == True:
+        print("Loop detected")
+        return True
 
 def part_a():
     # filename = 'sample.txt'
@@ -36,8 +147,8 @@ def part_a():
     guard_position = find_start_coord(grid)
     patrol_dir = 0
     steps = 0
-    print(f"gaurd position {guard_position}")
-    while guard_present and steps < 5:
+    # print(f"gaurd position {guard_position}")
+    while guard_present:
         if patrol_dir == 0: # guard moving up
             x = guard_position[0]
             y = guard_position[1]
@@ -104,22 +215,31 @@ def part_a():
                 if ele == '1':
                     position_count += 1
         print(position_count)
-    count_safe = 0
-    print(f"{DAY} Part A: {count_safe}")
-
+    print(f"{DAY} Part A: {position_count}")
 
 
 def part_b():
-    filename = 'sample.txt'
-    # filename = f'../inputs/{DAY}_input.txt'
-    reports = read_file(filename)
-    count_safe = 0
-    
-    print(f"{DAY} Part B: {count_safe}")
+    # filename = 'sample.txt'
+    filename = f'../inputs/{DAY}_input.txt'
+    grid = read_file(filename)
+    grid_size = len(grid)
+    loop_count = 0
+    for y in range(grid_size):
+        for x in range(grid_size):
+            grid = read_file(filename)
+            if grid[y][x] != '#' and grid[y][x] != '^':
+                grid[y][x] = '#'
+                # print(f"obstacle at {(x, y)}")
+                grid_dir = [[None for _ in range(grid_size)] for _ in range(grid_size)]
+                loop = traverse_grid(grid, grid_size, grid_dir, (x, y))
+                if loop:
+                    loop_count += 1
+    print(f"{DAY} Part B: {loop_count}")
+
 
 def main():
-    part_a()
-    # part_b()
+    # part_a()
+    part_b()
 
 if __name__ == "__main__":
     main()
---
+++ day7/code.py
@@ -0,0 +1,78 @@
+import re
+import itertools
+import operator
+
+DAY = 'day_7'
+
+
+def read_file(filename):
+    """Reads the contents of a file and returns it as a list of lines."""
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    return lines
+
+def split_lines(lines):
+    line_numbers = []
+    for line in lines:
+        nums = list(map(int, re.findall(r'\d+', line)))
+        line_numbers.append(nums)
+    return line_numbers
+
+
+def evaluate_line(lines):
+    ops = {
+        '+': operator.add,
+        '*': operator.mul,
+    }
+    sum_found = 0
+
+    for line in lines:
+        target_result = line[0]
+        nums = line[1:]  # Order remains the same
+        
+        # Generate combinations of operations
+        operations = list(itertools.product(ops.keys(), repeat=len(nums) - 1))
+
+        for ops_combo in operations:
+            try:
+                # Start with the first number
+                result = nums[0]
+                
+                # Apply operations in sequence
+                for i, op in enumerate(ops_combo):
+                    result = ops[op](result, nums[i + 1])
+                
+                # Check if the result matches the target
+                if result == target_result:
+                    sum_found += target_result
+                    print(f"{target_result} - match found")
+                    break  # No need to check further combinations
+            except ZeroDivisionError:
+                # Skip invalid operations
+                continue
+
+    return sum_found
+
+    
+def part_a():
+    filename = 'sample.txt'
+    filename = f'../inputs/{DAY}_input.txt'
+    lines = read_file(filename)
+    line_numbers = split_lines(lines)
+    sum_found = evaluate_line(line_numbers)
+    print(f"{DAY} Part A: {sum_found}")
+
+def part_b():
+    filename = 'sample.txt'
+    filename = f'../inputs/{DAY}_input.txt'
+    reports = read_file(filename)
+    count_safe = 0
+    
+    print(f"{DAY} Part B: {count_safe}")
+
+def main():
+    part_a()
+    # part_b()
+
+if __name__ == "__main__":
+    main()
